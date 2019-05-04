64ecba2e0cb5c44417bb81eafb58ba611ae8588b
torres <e.torres.menendez@gmail.com>
2019-05-04 18:15:14 +0200

    Initial commit

---
+++ db/init/01-filladb.sh
@@ -0,0 +1,59 @@
+#!/bin/bash
+
+set -o errexit
+
+
+# Creating an array that defines the environment variables
+# that must be set. This can be consumed later via arrray
+# variable expansion ${REQUIRED_ENV_VARS[@]}.
+readonly REQUIRED_ENV_VARS=(
+  "DB_USER"
+  "DB_PASSWORD"
+  "CONFIG_DB"
+  "DEFAULT_DB"
+  "POSTGRES_USER")
+
+
+# Main execution:
+# - verifies if all environment variables are set
+# - runs the SQL code to create user and database
+main() {
+  check_env_vars_set
+  init_user_and_db
+}
+
+
+# Checks if all of the required environment
+# variables are set. If one of them isn't,
+# echoes a text explaining which one isn't
+# and the name of the ones that need to be
+check_env_vars_set() {
+  for required_env_var in ${REQUIRED_ENV_VARS[@]}; do
+    if [[ -z "${!required_env_var}" ]]; then
+      echo "Error:
+    Environment variable '$required_env_var' not set.
+    Make sure you have the following environment variables set:
+      ${REQUIRED_ENV_VARS[@]}
+Aborting."
+      exit 1
+    fi
+  done
+}
+
+
+# Performs the initialization in the already-started PostgreSQL
+# using the preconfigured POSTGRE_USER user.
+init_user_and_db() {
+  psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<-EOSQL
+     CREATE USER $DB_USER WITH PASSWORD '$DB_PASSWORD';
+     CREATE DATABASE $CONFIG_DB;
+     GRANT ALL PRIVILEGES ON DATABASE $CONFIG_DB TO $DB_USER;
+     CREATE DATABASE $DEFAULT_DB;
+     GRANT ALL PRIVILEGES ON DATABASE $CONFIG_DB TO $DB_USER;
+EOSQL
+}
+
+# Executes the main routine with environment variables
+# passed through the command line. We don't use them in
+# this script but now you know 🤓
+main "$@"
